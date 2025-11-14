21a67b4d8a938c3c33cdbff5e46106a5d3a1617d
“Tom <tom.kihlborg@gmail.com>
2025-11-14 09:42:21 +0100

    added enter test

--- cypress/e2e/todo.cy.ts
+++ cypress/e2e/todo.cy.ts
@@ -15,3 +15,21 @@ describe('Todo tests', () => {
     cy.get("li").last().should("contain.text", text);
   });
 });
+
+describe('Todo tests', () => {
+  beforeEach(() => {
+    cy.visit("/")
+  })
+  it('should add todo by pressing enter', () => {
+    //assign
+    const text = "test success";
+    //const numberOfLisAtStart = cy.get("li").
+
+    //act
+    cy.get("input#todoText").type(`${text}{enter}`);
+    cy.get("button#save").click();
+
+    //assert
+    cy.get("li").last().should("contain.text", text);
+  });
+});
